7bc70a85985f8dc34e6fc69fa40bdefe953d41c0
ArseBro228 <iaseshviliarsen@gmail.com>
2022-02-11 21:22:30 +0200

    Lesson_37

---
+++ main.js
@@ -0,0 +1,123 @@
+// For1
+// let K = Number(prompt(`K`))
+// let N = Number(prompt(`N`))
+// for (let mean = 0; mean < N; mean++ ){
+//     console.log(K)
+// }
+
+// For2
+// let a = Number(prompt(`a`))
+// let b = Number(prompt(`b`))
+// for (let N = 1; a<=b; a++, N++){
+//     console.log(a, N)
+// }
+
+// For3
+// let a = Number(prompt(`a`))
+// let b = Number(prompt(`b`))-1
+// for (let N = 3; a<b; b--, N++){
+//     console.log(b, N)
+// }
+
+// For4
+// c = Number(prompt(`цена за 1кг`))
+// for (let a = 1; a <= 10; a ++ ){
+//     console.log(`Стоимость за ${a} кг, состовляет ${c*a}`)
+// }
+
+// For5
+// c = Number(prompt(`цена за 1кг`))
+// for (let a = 0.1; a <= 1; a += 0.1 ){
+//     console.log(`Стоимость за ${a} кг, состовляет ${c*a}`)
+// }
+
+// For6
+// c = Number(prompt(`цена за 1кг`))
+// for (let a = 1.2; a <= 2; a += 0.2 ){
+//     console.log(`Стоимость за ${a} кг, состовляет ${c*a}`)
+// }
+
+// For7
+// let a = Number(prompt(`a`))
+// let b = Number(prompt(`b`))
+// for (let N = 0; a<=b; a++){
+//     N = N+a
+//     console.log(N)
+// }
+
+// For8
+// let a = Number(prompt(`a`))
+// let b = Number(prompt(`b`))
+// for (let N = 1; a<=b; a++){
+//     N = N*a
+//     console.log(N)
+// }
+
+// For9
+// let a = Number(prompt(`a`))
+// let b = Number(prompt(`b`))
+// for (let N = 0; a<=b; a++){
+//     N = N+a**2
+//     console.log(N)
+// }
+
+// For10
+// summ = 0
+// let N = Number(prompt(`N`))
+// for (let a = 1; a<=N; a++){
+//     summ = (1/a) + summ
+//     console.log(summ)
+// }
+
+// For11
+// let summ = 0
+// let N = Number(prompt(`N`))
+// for (let i = 0; i<=N; i++ ){
+//     summ += (N+i)**2
+// }
+// console.log(summ)
+// For12
+// summ = 1
+// let N = Number(prompt(`N`))
+// for (let a = 1; a<=N; a += 0.1){
+//     summ = a * summ
+//     console.log(summ)
+// }
+
+// For13
+// let N = Number(prompt(`N`))
+// let summ = 0
+// for (let i = 0; i<=10*(N-1); i++){
+//     let v = (1+(i/10))*(-1)**i
+//     summ += v
+// }
+// console.log(summ)
+
+// For14\
+// let summ = 0
+// let N = Number(prompt(`N`))
+// for(let i = 1; i<=N; i++){
+//     summ += (2*i-1)
+// }
+// console.log(summ)
+
+// For15
+// let v = 1
+// let a = Number(prompt(`a`))
+// let N = Number(prompt(`N`))
+// for(let m = 0; m<N; m++){
+//     v*=a
+// }
+// console.log(v)
+
+// For16
+let v = 1
+let a = Number(prompt(`a`))
+let N = Number(prompt(`N`))
+for(let m = 0; m<N; m++){
+    v*=a
+    console.log(v)
+}
+
+
+
